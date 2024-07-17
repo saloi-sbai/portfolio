4d6c070b188117eb023798ee88c5786b2aa83547
saloi sbai <saloi.benoum@gmail.com>
2024-07-17 22:01:52 +0200

    init projet 12

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer id="footer" role="contentinfo">
+    <?php
+    wp_nav_menu(array(
+        'footer menu' => 'footer_menu',
+        'container' => 'false',
+        'menu_class' => 'footer-nav',
+    ));
+    ?>
+
+</footer>
+
+
+
+<?php wp_footer() ?>
+
+</body>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php get_header() ?>
+
+
+<h1>salam</h1>
+
+<?php get_footer() ?>
