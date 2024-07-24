09581eb522db9a75db4edb5e19c28bd0ecac8c7d
saloi sbai <saloi.benoum@gmail.com>
2024-07-24 19:45:36 +0200

    styles single projet

--- single-projet copy.php
+++ single-projet copy.php
@@ -17,43 +17,39 @@ if (have_posts()) :
                         ?>
                     </div>
                     <div class="link">
-                        <a title="Voir le code source" class="tooltip" href="<?php echo esc_url(get_post_meta($post->ID, 'link', true)); ?>" target="_blank"> <i class="fa-brands fa-github"></i></a>
+                        <a href="<?php echo esc_url(get_post_meta($post->ID, 'link', true)); ?>" target="_blank">Voir le projet</a>
                     </div>
                 </div>
             </div>
 
-            <!-- images projet -->
             <div class="illustration_projet">
                 <img src="<?php echo esc_url(wp_get_attachment_image_url(get_post_thumbnail_id(), 'desktop-home')); ?>" alt="<?php echo esc_attr(get_the_title()); ?>" class="photo-image">
             </div>
+            <div class="navigation">
+                <div class="next">
+                    <?php
+                    if (!empty(next_post_link('%link'))) {
+                        echo '<i class="fa-solid fa-arrow-left"></i>';
+                        next_post_link('%link');
+                    };
+                    ?>
+                </div>
+
+                <div class="home_page">
+                    <a href="<?php echo esc_url(home_url()); ?>">Retour à l'accueil</a>
+                </div>
+
+                <div class="previous">
+
+                    <?php
+                    if (!empty(previous_post_link('%link'))) {
+                        echo '<i class="fa-solid fa-arrow-right"></i>';
+                        previous_post_link('%link');
+                    };
+                    ?>
+                </div>
+            </div>
         </article>
-
-        <!-- partie navigation -->
-        <div class="navigation">
-            <div class="next">
-                <?php
-                if (!empty(next_post_link('%link'))) {
-                    echo '<i class="fa-solid fa-arrow-left"></i>';
-                    next_post_link('%link');
-                };
-                ?>
-            </div>
-
-            <div class="home_page">
-                <a href="<?php echo esc_url(home_url()); ?>">Retour à l'accueil</a>
-            </div>
-
-            <div class="previous">
-
-                <?php
-                if (!empty(previous_post_link('%link'))) {
-                    echo '<i class="fa-solid fa-arrow-right"></i>';
-                    previous_post_link('%link');
-                };
-                ?>
-            </div>
-        </div>
-
 <?php endwhile;
 else :
     echo '<p>No content found</p>';
