201284fb381391ddea7274e47c1c6506923d1748
saloi sbai <saloi.benoum@gmail.com>
2024-07-28 18:23:42 +0200

    menu burger ok

--- assets/scripts/burger-menu.js
+++ assets/scripts/burger-menu.js
@@ -1,23 +1,30 @@
 console.log("Le JS du menu burger s'est correctement chargé");
-$(document).ready(function () {
-  const menuBurger = $(".burgerMenu");
-  const nav = $(".main-menu");
 
-  menuBurger.on("click", function () {
-    const isOpen = nav.hasClass("open");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBurger = document.querySelector(".burgerMenu");
+  const nav = document.querySelector(".main-menu");
+
+  if (!menuBurger || !nav) {
+    console.error("Menu burger or navigation elements not found");
+    return; // Exit if elements are not found
+  }
+
+  menuBurger.addEventListener("click", function (event) {
+    event.preventDefault();
+    const isOpen = nav.classList.contains("open");
 
     if (isOpen) {
       // Si le menu est ouvert, ajoute la classe 'close' pour déclencher l'animation de fermeture
-      nav.addClass("close");
-      menuBurger.removeClass("open"); // Retirez la classe 'open' pour afficher les barres
+      nav.classList.add("close");
+      menuBurger.classList.remove("open"); // Retirez la classe 'open' pour afficher les barres
       setTimeout(() => {
         // Après un délai, retire la classe 'open' et 'close' pour réinitialiser l'animation
-        nav.removeClass("open close");
+        nav.classList.remove("open", "close");
       }, 1000); // Assurez-vous que le délai correspond à la durée de l'animation de fermeture
     } else {
       // Si le menu est fermé, ajoute la classe 'open' pour déclencher l'animation d'ouverture
-      nav.addClass("open");
-      menuBurger.addClass("open"); // Ajoutez la classe 'open' pour afficher la croix
+      nav.classList.add("open");
+      menuBurger.classList.add("open"); // Ajoutez la classe 'open' pour afficher la croix
     }
   });
 });
